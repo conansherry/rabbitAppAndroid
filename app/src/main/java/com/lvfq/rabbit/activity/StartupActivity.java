bd2b40d57a78bf7a1a7aff53255a1683d4715238
conansherry <conansherry@vip.qq.com>
2015-06-02 15:43:03 +0800

    尝试添加自定义开始画面= =未成功

--- app/src/main/java/com/lvfq/rabbit/activity/StartupActivity.java
+++ app/src/main/java/com/lvfq/rabbit/activity/StartupActivity.java
@@ -2,10 +2,20 @@ package com.lvfq.rabbit.activity;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.lvfq.rabbit.R;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.assist.ImageScaleType;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
 
 public class StartupActivity extends Activity {
 
@@ -18,16 +28,15 @@ public class StartupActivity extends Activity {
         super.onCreate(icicle);
         setContentView(R.layout.splashscreen);
 
-        /* New Handler to start the Menu-Activity
-         * and close this Splash-Screen after some seconds.*/
-        new Handler().postDelayed(new Runnable(){
+        new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                /* Create an Intent that will start the Menu-Activity. */
+                        /* Create an Intent that will start the Menu-Activity. */
                 Intent mainIntent = new Intent(StartupActivity.this, MainActivity.class);
                 StartupActivity.this.startActivity(mainIntent);
                 StartupActivity.this.finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
+
     }
 }
